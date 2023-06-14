570cccc36317347421b256ecc0994e5ecd481325
Fabian Huch <huch@in.tum.de>
2023-06-14 15:08:02 +0200

    updated jedit plugin;

--- search-jedit/src/findfacts_dockable.scala
+++ search-jedit/src/findfacts_dockable.scala
@@ -1,59 +1,117 @@
 package isabelle.jedit_findfacts
 
-import isabelle.jedit.{Dockable, PIDE, Pretty_Text_Area}
-import isabelle.{Command, GUI_Thread, Pretty, Session, XML}
+import java.awt.BorderLayout
 
+import scala.swing.{Component, Label}
+
+import de.qaware.findfacts.core
+import de.qaware.findfacts.common.dt.EtField
+import de.qaware.findfacts.core.{FieldFilter, FilterQuery}
+import isabelle.jedit.*
+import isabelle.*
 import org.gjt.sp.jedit.View
+import org.gjt.sp.jedit.gui.HistoryTextField
+import java.awt.event.KeyEvent
 
 
-class Findfacts_Dockable(view: View, position: String) extends Dockable(view, position)
-{
+
+class Findfacts_Dockable(view: View, position: String) extends Dockable(view, position) {
   GUI_Thread.require {}
 
-  /* component state -- owned by GUI thread */
-
-  private var current_output: List[XML.Tree] = Nil
-
-  /* pretty text area */
+  /* text area */
 
   val pretty_text_area = new Pretty_Text_Area(view)
+  set_content(pretty_text_area)
+
+  // panel state: indexed snapshot + query
+  private var _state: Option[(Document.Version, String)] = None
 
   override def detach_operation: Option[() => Unit] = pretty_text_area.detach_operation
 
-  private def handle_update(): Unit =
-  {
+  private def handle_update(): Unit = {
     GUI_Thread.require {}
 
     for {
+      snapshot <- PIDE.maybe_snapshot()
+      if !snapshot.is_outdated
       plugin <- Findfacts_Plugin.instance
-      (snapshot, output) <- plugin.findfacts.state
+      state <- plugin.findfacts.state
+      if _state != Some(state, query.getText)
     } {
-      val new_output = List(XML.Text(output))
-      if (new_output != current_output) {
-        pretty_text_area.update(snapshot, Command.Results.empty, Pretty.separate(new_output))
-        current_output = new_output
+      if (!query.getText.isBlank) search()
+      else {
+        val text = "Updated to " + snapshot.version.id.toString
+        pretty_text_area.update(snapshot, Command.Results.empty, Pretty.separate(List(XML.Text(text))))
+        _state = Some(state, "")
       }
     }
   }
 
-  /* panel */
+  /* controls */
 
-  set_content(pretty_text_area)
+  private def search(): Unit = {
+    val q = query.getText
+    val f_q = FilterQuery(List(FieldFilter(EtField.Name, core.Term(q))))
+    for {
+      snapshot <- PIDE.maybe_snapshot()
+      if !snapshot.is_outdated
+      plugin <- Findfacts_Plugin.instance
+      state <- plugin.findfacts.state
+      findfacts = plugin.findfacts.search_service
+      indexes <- findfacts.listIndexes
+      index <- indexes.headOption
+      result <- findfacts.getResultShortlist(f_q)(index)
+    } {
+      val text =
+        "Indexes: " + commas_quote(indexes) + "\n" +
+        "Results for " + quote(q) + ": " + result.count + "\n"
+      pretty_text_area.update(snapshot, Command.Results.empty, Pretty.separate(List(XML.Text(text))))
+      _state = Some(state, query.getText)
+    }
+  }
+
+  private val query_label = new Label("Query:") {
+    tooltip =
+      GUI.tooltip_lines(
+        "Findfacts search query")
+  }
+
+  private val query = new HistoryTextField("findfacts-query") {
+    override def processKeyEvent(evt: KeyEvent): Unit = {
+      if (evt.getID == KeyEvent.KEY_PRESSED && evt.getKeyCode == KeyEvent.VK_ENTER) search()
+      super.processKeyEvent(evt)
+    }
+
+    setToolTipText(query_label.tooltip)
+    setColumns(30)
+  }
+
+  private val apply_query = new GUI.Button("<html><b>Search</b></html>") {
+    tooltip = "Search FindFacts"
+
+    override def clicked(): Unit = search()
+  }
+
+  private val controls =
+    Wrap_Panel(
+      List(query_label, Component.wrap(query), apply_query))
+
+  add(controls.peer, BorderLayout.NORTH)
+
 
   /* main */
 
-  private val main =
-    Session.Consumer[Any](getClass.getName) {
-      case _ => GUI_Thread.later(handle_update())
-    }
+  private val main = Session.Consumer[Any](getClass.getName)(_ => GUI_Thread.later(handle_update()))
 
   override def init(): Unit = {
     PIDE.session.global_options += main
-    PIDE.session.caret_focus += main
+    PIDE.session.commands_changed += main
+    PIDE.session.finished_theories += main
   }
 
   override def exit(): Unit = {
     PIDE.session.global_options -= main
-    PIDE.session.caret_focus -= main
+    PIDE.session.commands_changed -= main
+    PIDE.session.finished_theories -= main
   }
 }
--- search-jedit/src/findfacts_variable.scala
+++ search-jedit/src/findfacts_variable.scala
@@ -1,7 +1,9 @@
 package isabelle.jedit_findfacts
 
+
+import isabelle._
+
 import de.qaware.findfacts._
-import de.qaware.findfacts.importer.Importer
 import de.qaware.findfacts.common.solr._
 import de.qaware.findfacts.core.{FacetQuery, QueryService}
 import de.qaware.findfacts.core.solrimpl.{SolrFieldFilterMapper, SolrFilterMapper, SolrQueryMapper, SolrQueryService}
@@ -10,17 +12,16 @@ import isabelle.jedit.PIDE
 import org.slf4j.LoggerFactory
 
 
-class Findfacts_Variable
-{
+class Findfacts_Variable {
   private val solr_dir = Isabelle_System.tmp_dir("findfacts")
   private val repo: SolrRepository = LocalSolr(solr_dir)
 
-  private var _state: Option[(Document.Snapshot, String)] = None
-  def state: Option[(Document.Snapshot, String)] = _state
+  private var _state: Option[Document.Version] = None
+  def state: Option[Document.Version] = _state
 
   val search_service: QueryService = new SolrQueryService(repo, new SolrQueryMapper(new SolrFieldFilterMapper(new SolrFilterMapper())))
 
-  val logger = new Console_Progress()
+  val progress = new Console_Progress()
 
   private val main =
     Session.Consumer[Any](getClass.getName) {
@@ -28,38 +29,38 @@ class Findfacts_Variable
         for {
           snapshot <- PIDE.maybe_snapshot()
           theory_name = snapshot.node_name.theory
-          if (!snapshot.is_outdated)
-          if (snapshot.version.nodes.theory_name(theory_name).isDefined)
+          if !snapshot.is_outdated
+          if snapshot.version.nodes.theory_name(theory_name).isDefined
         } {
-          logger.echo("Importing " + theory_name)
-          val index_name = snapshot.version.nodes.topological_order.mkString("_") + "_" + snapshot.version.id
+          progress.echo("Importing " + theory_name)
+          val index_name = "Local"
           if (!repo.listIndexes.contains(index_name)) {
-            logger.echo("Creating index " + index_name)
+            progress.echo("Creating index " + index_name)
             repo.createIndex(index_name)
           }
 
+          val store = Sessions.store(PIDE.options.value)
           val base = PIDE.resources.session_base_info
-          using(Export.Session_Context.open_session(base, Some(snapshot))) { session_context =>
-            logger.echo("Importing..")
-            Build_Importer.solr_import(index_name, session_context, repo)
+
+          using(Export.open_session_context(store, base, Some(snapshot))) { session_context =>
+            // TODO proper import
           }
-          // TODO delete old index
-          logger.echo("Finished importing")
-          _state = Some((snapshot, "Updated to revision " + snapshot.version))
+
+          _state = Some(snapshot.version)
 
           PIDE.session.caret_focus.post(Session.Caret_Focus)
         }
     }
 
-  def install_handlers(): Unit =
-  {
+  def install_handlers(): Unit = {
     PIDE.session.global_options += main
     PIDE.session.commands_changed += main
+    PIDE.session.finished_theories += main
   }
 
-  def uninstall_handlers(): Unit =
-  {
+  def uninstall_handlers(): Unit = {
     PIDE.session.global_options -= main
     PIDE.session.commands_changed -= main
+    PIDE.session.finished_theories -= main
   }
 }
