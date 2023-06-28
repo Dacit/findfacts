5782e004d55f7878bdcfca2f386aacb686f3fd7b
Fabian Huch <huch@in.tum.de>
2023-06-28 09:58:31 +0200

    proper support for draft theories with same name;

--- search-jedit/src/findfacts_variable.scala
+++ search-jedit/src/findfacts_variable.scala
@@ -17,7 +17,7 @@ import de.qaware.findfacts.common.solr.*
 import de.qaware.findfacts.common.dt.EtField
 import de.qaware.findfacts.core.QueryService.ResultList
 import de.qaware.findfacts.core.dt.ShortBlock
-import de.qaware.findfacts.core.{Exact, FacetQuery, FieldFilter, FilterQuery, QueryService}
+import de.qaware.findfacts.core.{Exact, FacetQuery, FieldFilter, Filter, FilterQuery, QueryService}
 import de.qaware.findfacts.core.solrimpl.{SolrFieldFilterMapper, SolrFilterMapper, SolrQueryMapper, SolrQueryService}
 import de.qaware.findfacts.importer.ImporterModule
 import de.qaware.findfacts.importer.solrimpl.SolrImporterModule
@@ -36,10 +36,9 @@ object Findfacts_Variable {
   class Context(
     importer: ImporterModule,
     service: QueryService,
-    private var _indexed_theories: Map[(String, String), SHA1.Shasum]
+    private var _indexed_theories: Map[Context.Node, SHA1.Shasum]
   ) {
-    import isabelle.jedit_findfacts.Findfacts_Variable.Context.DRAFT_SHASUM
-    import isabelle.jedit_findfacts.Findfacts_Variable.Context.INDEX
+    import isabelle.jedit_findfacts.Findfacts_Variable.Context.*
 
     private def do_import(
       session_name: String,
@@ -59,9 +58,9 @@ object Findfacts_Variable {
        session_context: Export.Session_Context,
        snapshot: Document.Snapshot,
      ): Unit = {
-      val base_name = Long_Name.base_name(node_name.theory)
+      val node = Node(session_name, node_name)
       val do_update =
-        _indexed_theories.get(session_name, base_name) match {
+        _indexed_theories.get(node) match {
           case Some(DRAFT_SHASUM) => snapshot.state.node_initialized(snapshot.version, node_name)
           case Some(_) => false
           case None => true
@@ -70,14 +69,14 @@ object Findfacts_Variable {
       if (do_update) {
         service.deleteBlock(List(
           FieldFilter(EtField.SessionFacet, Exact(session_name)),
-          FieldFilter(EtField.SourceTheoryFacet, Exact(base_name)),
+          FieldFilter(EtField.SourceTheoryFacet, Exact(node.theory_base_name)),
           FieldFilter(EtField.Version, Exact(DRAFT_SHASUM.toString))))
 
         val xml = snapshot.state.xml_markup(snapshot.version, node_name)
         val theory_context = session_context.theory(node_name.theory)
         do_import(session_name, DRAFT_SHASUM, node_name, theory_context, xml)
 
-        _indexed_theories = _indexed_theories + ((session_name, base_name) -> DRAFT_SHASUM)
+        _indexed_theories = _indexed_theories + (node -> DRAFT_SHASUM)
       }
     }
 
@@ -94,35 +93,68 @@ object Findfacts_Variable {
           if session_context.sessions_structure.theory_qualifier(theory_name) == session_name
         } yield theory_name
 
-      def is_imported(theory_name: String): Boolean =
-        _indexed_theories.get(session_name, Long_Name.base_name(theory_name)).contains(info.meta_info)
+      val node_names =
+        proper_theories.flatMap(theory_name => PIDE.resources.find_theory_node(theory_name).map(theory_name -> _)).toMap
+      val nodes = node_names.values.toList.map(Node(session_name, _))
 
-      if (!proper_theories.forall(is_imported)) {
-        for {
-          theory_name <- proper_theories
-          node_name <- PIDE.resources.find_theory_node(theory_name)
-        } {
-          val theory_context = session_context.theory(theory_name)
-          val xml = theory_context.yxml(Export.MARKUP)
-          do_import(session_name, info.meta_info, node_name, theory_context, xml)
+      val (indexed, missing) = nodes.partition(_indexed_theories.contains)
+      val (draft, regular) = indexed.partition(_indexed_theories.get(_).contains(DRAFT_SHASUM))
+      val old_version = regular.filterNot(_indexed_theories.get(_).contains(info.meta_info))
+
+      def theory_filter(node: Node): Filter = Exact(node.theory_base_name)
+      def theories_filter(nodes: List[Node]): Option[Filter] = nodes match{
+        case Nil => None
+        case node :: Nil => Some(theory_filter(node))
+        case node1 :: node2 :: nodes =>
+          Some(core.Or(theory_filter(node1), theory_filter(node2), nodes.map(theory_filter): _*))
+      }
+
+      theories_filter(draft).foreach(filter => service.deleteBlock(
+        List(
+          FieldFilter(EtField.SessionFacet, Exact(session_name)),
+          FieldFilter(EtField.SourceTheoryFacet, filter),
+          FieldFilter(EtField.Version, Exact(DRAFT_SHASUM.toString)))))
+
+      val import_nodes =
+        if (old_version.isEmpty) missing ::: draft
+        else {
+          service.deleteBlock(List(FieldFilter(EtField.Version, Exact(info.meta_info.toString))))
+          nodes
         }
 
-        _indexed_theories = _indexed_theories ++ proper_theories.map(theory_name =>
-          (session_name, Long_Name.base_name(theory_name)) -> info.meta_info)
+      for {
+        node <- import_nodes
+        theory_name = PIDE.resources.theory_name(session_name, node.theory_base_name)
+        node_name <- node_names.get(theory_name)
+      } {
+        val theory_context = session_context.theory(theory_name)
+        val xml = theory_context.yxml(Export.MARKUP)
+        do_import(session_name, info.meta_info, node_name, theory_context, xml)
       }
+
+      _indexed_theories = _indexed_theories ++ nodes.map(_ -> info.meta_info)
     }
 
     def search(query: FilterQuery): Try[ResultList[ShortBlock]] = service.getResultShortlist(query)(INDEX)
     def num_indexed: Int = _indexed_theories.size
     def debug_info: String =
       _indexed_theories.map {
-        case ((s,t),v) => s+"."+t+"-"+v.digest.toString
+        case (k,v) => k.toString + ": " + v.digest.toString
       }.mkString("\n")
   }
 
   object Context {
     private val DRAFT_SHASUM = SHA1.fake_shasum("Draft")
     private implicit val INDEX: String = "local"
+
+    case class Node(session_name: String, theory_base_name: String, file: Path) {
+      override def toString: String = session_name + "." + theory_base_name + " ("+ file.implode + ")"
+    }
+    object Node {
+      def apply(session_name: String, node_name: Document.Node.Name): Node =
+        new Node(session_name, node_name.theory_base_name, node_name.path)
+      def apply(block: ShortBlock): Node = new Node(block.session, block.theory, Path.explode(block.file))
+    }
 
     def is_draft(block: ShortBlock): Boolean = block.version == DRAFT_SHASUM.toString
 
@@ -148,8 +180,7 @@ object Findfacts_Variable {
           service.deleteBlock(List(FieldFilter(EtField.SourceFile, core.Or(elem1, elem2, elems: _*))))
       }
 
-      val indexed = present.map(block =>
-        (block.session.toString, block.theory.toString) -> SHA1.fake_shasum(block.version)).toMap
+      val indexed = present.map(block => Node(block) -> SHA1.fake_shasum(block.version)).toMap
       new Context(importer, service, indexed)
     }
   }
